c22b427523fc7f7d5d9cf4e632e5c5502d55c329
Lufupa <lufupinha101@hotmail.com>
2017-08-25 22:10:22 -0300

    update

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.db import models
 
 
@@ -8,5 +9,3 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
 
-     def __str__(self):
-        return self.name

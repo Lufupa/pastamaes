2d7ae65cc6323ada5e8c4531c2ba87194f8b1fa1
Lufupa <lufupinha101@hotmail.com>
2017-08-25 23:07:46 -0300

    c

--- catalog/models.py
+++ catalog/models.py
@@ -9,6 +9,6 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20)
 
-     def __str__(self):
+     def__str__(self):
         return self.name
 
